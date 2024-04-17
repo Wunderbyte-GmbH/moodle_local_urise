10f7201f9e61ba30a4e95252abd4aff7d2abf7ad
Georg Maißer <georg.maisser@wunderbyte.at>
2024-04-17 17:36:21 +0200

    Version (2024030403)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_berta';
 $plugin->release = '0.1.3';
-$plugin->version = 2024030400;
+$plugin->version = 2024030403;
 $plugin->requires = 2022041900; // Requires this Moodle version. Current: Moodle 4.0.0.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
