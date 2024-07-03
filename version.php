bfbefe454141b5b2c451893576f5dd36b7c80430
cbadusch <hello@christianbadusch.codes>
2024-07-03 15:39:55 +0200

    relocate button scss to lexa

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_berta';
 $plugin->release = '0.1.3';
-$plugin->version = 2024061700;
+$plugin->version = 2024061701;
 $plugin->requires = 2022041900; // Requires this Moodle version. Current: Moodle 4.0.0.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
