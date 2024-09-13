212dd841a76b87b71244ced6f672be5f8b1a8d3d
Georg Maißer <georg.maisser@wunderbyte.at>
2024-09-13 14:01:05 +0200

    Fix staff string

--- lang/en/local_urise.php
+++ lang/en/local_urise.php
@@ -488,7 +488,7 @@ $string['phdstudents'] = "PhD Students";
 $string['postdoc'] = "Postdocs";
 $string['fuehrungskraefte'] = "Executive staff";
 $string['allgemeinespersonal'] = "General staff";
-$string['wissenschaftlichespersonal'] = "Academic university staff";
+$string['wissenschaftlichespersonal'] = "Academic staff";
 $string['studierende'] = "Students";
 $string['interessierteoeffentlichkeit'] = "General public";
 
