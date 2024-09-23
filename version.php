2272e4928f59ff3da0d4db60bb614cfb996f3c91
cbadusch <hello@christianbadusch.codes>
2024-09-23 10:42:29 +0200

    Version (2024092300)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_urise';
-$plugin->release = '0.2.14';
-$plugin->version = 2024091601;
+$plugin->release = '0.2.15';
+$plugin->version = 2024092300;
 $plugin->requires = 2022041900; // Requires this Moodle version. Current: Moodle 4.0.0.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
