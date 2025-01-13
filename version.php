84b9e0e1db4eda23ce1ae59acdb8bd5015c448b9
Georg Maißer <georg.maisser@wunderbyte.at>
2025-01-13 16:36:19 +0100

    Version 0.2.31 (2025011300)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_urise';
-$plugin->release = '0.2.30';
-$plugin->version = 2025010800;
+$plugin->release = '0.2.31';
+$plugin->version = 2025011300;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
