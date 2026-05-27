5b095709725d41d6f713ac6a791c0dbe73f28df6
Magdalena <78353570+eynimeni@users.noreply.github.com>
2026-05-27 08:41:32 +0200

    Version 0.2.45 (2026052700)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_urise';
-$plugin->release = '0.2.44';
-$plugin->version = 2026051900;
+$plugin->release = '0.2.45';
+$plugin->version = 2026052700;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
