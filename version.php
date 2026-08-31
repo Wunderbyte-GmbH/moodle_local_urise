154e8989267f690e868d8ccf9d2c9b3fa7cbb45b
David Ala <david.ala@wunderbyte.at>
2026-08-31 14:34:46 +0200

    Bugfix: new capability enforcement handeled Wunderbyte-GmbH/Wunderbyte-GmbH#2327

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_urise';
-$plugin->release = '0.2.51';
-$plugin->version = 2026080600;
+$plugin->release = '0.2.52';
+$plugin->version = 2026083100;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
