9a9534bafcf45b021ed021ff4bd7c87464ac8728
David Ala <david.ala@wunderbyte.at>
2026-09-07 12:32:23 +0200

    Version (2026090700)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_urise';
 $plugin->release = '0.2.53';
-$plugin->version = 2026090200;
+$plugin->version = 2026090700;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
